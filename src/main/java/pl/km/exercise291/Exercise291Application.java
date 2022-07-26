de991a14eb491850f9c8d6ce771aa603a00f84e9
kuba <kubamachnik@gmail.com>
2022-07-26 13:42:00 +0200

    exercise 19.1 - project initialization

---
+++ src/main/java/pl/km/exercise291/Exercise291Application.java
@@ -0,0 +1,13 @@
+package pl.km.exercise291;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise291Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise291Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/km/exercise291/Exercise291ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.km.exercise291;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise291ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
